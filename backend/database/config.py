6e3ce7abdd667cfde98dc0a5d1300284bdf7011f
Henrik Au-Young <auhenrik@gmail.com>
2025-06-23 12:18:28 +0800

    quick fix to db

--- backend/database/config.py
+++ backend/database/config.py
@@ -2,10 +2,12 @@ import os
 from sqlmodel import create_engine
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql+psycopg2://postgres:your_password@localhost:5432/pokecard"
 )
+print("DATABASE_URL:", DATABASE_URL)
 engine = create_engine(DATABASE_URL, echo=True)
